3c7f39e978770a55b533f573f089419a7629643b
kukujiabo <kukujiabo@163.com>
2018-05-05 18:14:55 +0800

    新增项目经理接口

---
+++ src/app/Domain/ManagerDm.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Domain;
+
+/**
+ * 项目经理
+ *
+ */
+class ManagerDm {
+
+  public function addManager($data) {
+  
+    return \App\request('App.Manager.AddManager', $data);
+  
+  }
+
+  public function getList($data) {
+  
+    return \App\request('App.Manager.GetList', $data);
+  
+  }
+
+}
