0205b7e66f04ceb05016169413b7e87aef775968
kukujiabo <kukujiabo@163.com>
2018-05-14 17:29:05 +0800

    123

--- src/app/Api/GoodsProviderCos.php
+++ src/app/Api/GoodsProviderCos.php
@@ -15,12 +15,11 @@ class GoodsProviderCos extends BaseApi {
       'addGoodsCos' => array(
       
         'token' => 'token|string|true||管理员令牌',
-        'sku_id' => 'sku_id|int|true||商品skuid',
         'goods_id' => 'goods_id|int|true||商品id',
         'provider_id' => 'provider_id|int|true||供应商id',
         'cost' => 'cost|float|true||商品进价',
         'goods_name' => 'goods_name|int|true||商品skuid',
-        'sku_name' => 'sku_name|string|true||商品skuid'
+        'skus' => 'skus|string|true||商品规格'
       
       )
     
