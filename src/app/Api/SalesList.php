666d322bbe6fba60e2b43e05c32ea6488baac081
kukujiabo <kukujiabo@163.com>
2018-07-26 14:32:09 +0800

    123

--- src/app/Api/SalesList.php
+++ src/app/Api/SalesList.php
@@ -16,6 +16,7 @@ class SalesList extends BaseApi {
       
         'token' => 'token|string|true||令牌',
         'manager_account' => 'manager_account|string|true||销售总监账号',
+        'user_tel' => 'user_tel|string|false||手机号',
         'page' => 'page|int|false||页码',
         'page_size' => 'page_size|int|false||每页条数'
       
