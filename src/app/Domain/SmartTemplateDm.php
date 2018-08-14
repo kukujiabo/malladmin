a3d89070f4c134d5d8e7b5d4fad3bfb94f368d63
kukujiabo <kukujiabo@163.com>
2018-08-15 04:03:38 +0800

    123

--- src/app/Domain/SmartTemplateDm.php
+++ src/app/Domain/SmartTemplateDm.php
@@ -5,13 +5,13 @@ class SmartTemplateDm {
 
   public function create($data) {
   
-    return \App\request('App.SmartTemplate.Create', $params);
+    return \App\request('App.SmartTemplate.Create', $data);
   
   }
 
   public function getList($data) {
   
-    return \App\request('App.SmartTemplate.GetList', $params);
+    return \App\request('App.SmartTemplate.GetList', $data);
   
   }
 
