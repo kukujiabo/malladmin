fe22fecaa26ba8c467ec55481aa0acffa7a87ca9
kukujiabo <kukujiabo@163.com>
2018-08-26 23:04:41 +0800

    123

---
+++ src/app/Api/DataManager.php
@@ -0,0 +1,63 @@
+<?php
+namespace App\Api;
+
+/**
+ * 数据员接口
+ *
+ *
+ */
+class DataManager extends BaseApi {
+
+  public function getRules() {
+  
+    return $this->rules(array(
+    
+      'create' => array(
+      
+        'name'  => 'name|string|true||姓名',
+
+        'mobile'  => 'mobile|string|true||手机号',
+
+        'city_code'  => 'city_code|string|true||城市编码'
+      
+      ),
+
+      'getList' => array(
+      
+        'name'  => 'name|string|false||姓名',
+
+        'mobile'  => 'mobile|string|false||手机号',
+
+        'city_code'  => 'city_code|string|false||城市编码'
+      
+      )
+    
+    ));
+  
+  }
+
+  /**
+   * 新增数据员
+   * @desc 新增数据员
+   *
+   * @return int id
+   */
+  public function create() {
+  
+    return $this->dm->create($this->retriveRuleParams(__FUNCTION__));  
+  
+  }
+
+  /**
+   * 获取数据员列表
+   * @desc 获取数据员列表
+   *
+   * @return array list
+   */
+  public function getList() {
+  
+    return $this->dm->getList($this->retriveRuleParams(__FUNCTION__)); 
+  
+  }
+
+}
---
+++ src/app/Domain/DataManagerDm.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Domain;
+
+class DataManagerDm {
+
+  public function create($data) {
+  
+    return \App\request('App.DataManager.Create', $data); 
+  
+  }
+
+  public function getList($data) {
+  
+    return \App\request('App.DataManager.GetList', $data); 
+  
+  }
+
+}
