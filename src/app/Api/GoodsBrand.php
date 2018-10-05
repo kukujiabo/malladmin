e6604719a6e80ffddb0d5889d310d1bac8dcca9b
kukujiabo <kukujiabo@163.com>
2018-10-05 12:56:19 +0800

    123

--- src/app/Api/GoodsBrand.php
+++ src/app/Api/GoodsBrand.php
@@ -56,6 +56,7 @@ class GoodsBrand extends BaseApi {
         'brand_code' => 'brand_code|string|false||品牌代码',
         'brand_state' => 'brand_state|int|false||品牌状态',
         'index_show' => 'index_show|int|false||首页展示',
+        'category_id' => 'category_id|int|false||分类id',
         'all' => 'all|int|false|0|是否查询全部',
         'page' => 'page|int|false|1|页码',
         'page_size' => 'page_size|int|false|20|每页条数'
